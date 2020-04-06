449062bd8c67c26c24f015a78657e25da13b5a2e
Edward Blanciak <blanciak.edward2@gmail.com>
2020-04-05 21:45:10 -0400

    second navbar version

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,31 +4,67 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   const dropDownHandler = (event) => {
     const list = document.querySelectorAll('a');
+    const clickMe = document.querySelector('.click-me');
+    const greeting = document.querySelector('.greeting');
+    const codeIcons = document.querySelectorAll('.codeIcons');
+
     list.forEach((item) => {
       item.classList.toggle('visible');
     });
+
+    codeIcons.forEach((item) => {
+      item.classList.toggle('hidden');
+    });
+    clickMe.classList.toggle('hidden');
+    greeting.classList.toggle('hidden');
+    console.log(codeIcons);
   };
 
   return (
     <>
       <div className="drop-down-icon" onClick={dropDownHandler}>
         <i className="fas fa-bars fa-5x"></i>
+        <div className="click-me">Click Me</div>
       </div>
       <nav>
+        <div className="greeting">
+          <i className="fab fa-html5 codeIcons"></i>
+          <i className="fab fa-css3-alt codeIcons"></i>
+          <i className="fab fa-js codeIcons"></i>
+          <i className="fab fa-react codeIcons"></i>
+        </div>
         <Link to="/">
-          <span>H </span>ome
+          <span>
+            <i className="fas fa-square"></i>
+          </span>
+          {/* <span>
+            <i class="far fa-square"></i>
+          </span> */}
+          home
         </Link>
         <Link to="/about">
-          <span>A </span>bout
+          <span>
+            <i className="far fa-square"></i>
+          </span>
+          about
         </Link>
         <Link to="/projects">
-          <span>P </span>rojects
+          <span>
+            <i className="far fa-square"></i>
+          </span>
+          projects
         </Link>
         <Link to="/resume">
-          <span>R </span>esume
+          <span>
+            <i className="far fa-square"></i>
+          </span>
+          resume
         </Link>
         <Link to="/contact">
-          <span>C </span>ontact
+          <span>
+            <i className="far fa-square"></i>
+          </span>
+          contact
         </Link>
       </nav>
     </>
